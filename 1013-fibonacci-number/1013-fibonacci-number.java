60a0203633da9958e8974403e33015427f09445a
Sanddesh Raja L S <76258935+sanddeshraja@users.noreply.github.com>
2023-05-09 22:05:12 +0530

    Time: 14 ms (7.62%), Space: 39.2 MB (80.13%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,19 +1,16 @@
 class Solution {
     public int fib(int n) {
-            return fiboCalc(n);
-        }
-    
-    public int fiboCalc(int n)
-    {
-        if(n == 0){
+          if(n == 0){
             return 0;
         }
         else if(n==1){
             return 1;
         }
         while(n>=2){
-        return fiboCalc(n-1) + fiboCalc(n-2);
+        return fib(n-1) + fib(n-2);
         }
         return 0;
-    }
+        }
+    
+ 
 }
