318195ab439e627db48d42a64b95c99450d26ddc
Sanddesh Raja L S <76258935+sanddeshraja@users.noreply.github.com>
2023-05-10 10:59:52 +0530

    Time: 422 ms (7.62%), Space: 41.7 MB (5.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int fib(int n) {
-          if(n<=1){
-            return n;
-             }   
-        else{
-        if(n>=2){
-        return fib(n-1) + fib(n-2);
-        }
-        return 0;
+          Map<Integer,Integer> map = new HashMap<>();
+          map.put(0,0);
+          map.put(1,1);
+            if(map.containsKey(n))
+                return map.get(n);
+            else{
+                map.put(n,fib(n-1) + fib(n-2));
+                return map.get(n);
+            }
         }
     }
-    
- 
-}
