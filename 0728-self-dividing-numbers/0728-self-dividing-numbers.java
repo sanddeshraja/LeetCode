5d1d8dd318a99c14640082c59fcdf03db6005c99
Sanddesh Raja L S <76258935+sanddeshraja@users.noreply.github.com>
2023-08-17 18:46:18 +0530

    Time: 3 ms (29.11%), Space: 40.6 MB (22.89%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.java
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.java
@@ -3,11 +3,14 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         int curr = left;
         while(left <= right){
-            int lastDigit;
+            int lastDigit = Integer.MIN_VALUE;
             boolean redFlag = false;
             while(curr > 0){
                 lastDigit = curr % 10;
-                if(lastDigit == 0 || !(left % lastDigit == 0)){
+                if(lastDigit == 0){
+                    break;
+                }
+                if(!(left % lastDigit == 0)){
                     redFlag = true;
                     break;
                 }else{
@@ -17,7 +20,7 @@ class Solution {
             int ans = left;
             curr = left + 1;
             left = curr;
-            if(redFlag == true) continue;
+            if(redFlag == true || lastDigit == 0) continue;
             list.add(ans);
         }
         return list;
