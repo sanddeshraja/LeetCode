8cd344b9763474b4c23b9243f7925117c39ea76a
Sanddesh Raja L S <76258935+sanddeshraja@users.noreply.github.com>
2023-08-20 23:24:48 +0530

    Time: 2 ms (100%), Space: 43.8 MB (12.92%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,32 +1,12 @@
 class Solution {
     public boolean judgeCircle(String moves) {
        
-         int point[] = {0,0};
-        int move[][] = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (Character m: moves.toCharArray()) {
-            int ind = 0;
-            switch (m)
-            {
-                case 'U': {
-                    ind = 0;
-                    break;
-                }
-                case 'D': {
-                    ind = 1;
-                    break;
-                }
-                case 'L': {
-                    ind = 2;
-                    break;
-                }
-                case 'R': {
-                    ind = 3;
-                    break;
-                }
-            }
-            point[0] += move[ind][0];
-            point[1] += move[ind][1];
-        }
-        return point[0] == 0 && point[1] == 0;
+           char[] ch = moves.toCharArray();
+        int[] alpha = new int[26];
+
+        for (char c: ch) alpha[(int)(c - 65)]++;
+
+        if (alpha[3] == alpha[20] && alpha[17] == alpha[11]) return true;
+        else return false;   
     }
 }
