ac436b250ac777a3904ca667f61173851adfc142
Sanddesh Raja L S <76258935+sanddeshraja@users.noreply.github.com>
2023-10-20 15:58:06 +0530

    Time: 0 ms (100%), Space: 44 MB (94.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-         int[] ans={-1,-1};
-         ans[0]=search(nums, target,true);
-         ans[1]=search(nums,target,false);
-         return ans;
-
-    }
-    int search (int[] nums,int target, boolean first)
-    {
         int i=0;
         int j=nums.length-1;
+        int fo=0;
+        int lo=0;
         while(i<=j)
         {
-            if(first)
+            int mid = i+(j-i)/2;
+            if(nums[mid]==target)
             {
-                if(nums[i]==target)
-                    {
-                        return i;
-                    }
-                else
-                 i++;
+                fo=find(nums, mid,target,true);
+                lo=find(nums, mid,target, false);
+                return new int[]{fo,lo};
+            }
+            else if(nums[mid]< target){
+                i=mid+1;
             }
             else
-            {
-                if(nums[j]==target)
-                {
-                    return j;
-                }
-                else
-                 j--;
-            }
+                j=mid-1;
         }
-        return -1;
-}
+        if(fo==0){
+            return new int[]{-1,-1};
+        }
+        else
+            return new int[]{fo,lo};
+    }
+    int find(int[] a, int mid, int target, boolean occurence){
+        int temp=-1;
+        while( a[mid]==target){
+            if(occurence){
+                temp=mid;
+                mid=mid-1;
+            }
+            else if(a[mid]==target)
+            {
+                temp=mid;
+                mid=mid+1;
+            }
+            if(mid == -1 || mid == a.length)
+                return temp;
+        }
+        return temp;
+    }
 }
